d116847ebb2f1417cb3bb3ab4e869498f1940ef0
udeilton de sousa <deilton_black@hotmail.com>
2022-06-25 01:06:42 -0300

    estudo

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapasso a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
+
