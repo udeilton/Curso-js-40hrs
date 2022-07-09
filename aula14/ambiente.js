f8e20bc5a358831b8cf5bc38b3a0728c2458fda3
udeilton de sousa <deilton_black@hotmail.com>
2022-07-09 00:56:40 -0300

    estudo

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/* var c = 1
+while (c <= 5) {
+    console.log(c)
+    c = c + 1
+}
+*/
+  console.log('Vai começar')
+  for (var c = 1 ; c <= 10 ; c++) {
+    console.log(c)
+
+  }
+   console.log('Fim')
