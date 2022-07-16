2bf38d50e01e0343259c62a21d6825bbe27cef83
udeilton de sousa <deilton_black@hotmail.com>
2022-07-15 22:26:12 -0300

    estudo

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,2,9,3]
+var pos = 0
+
+num.sort()
+for (let pos in num)
+console.log(`Nosso vetor é o ${num[pos]}`)
+num.indexOf(9)
+var pro = num.indexOf(9)
+
+console.log(`O index é ${pro}`)
